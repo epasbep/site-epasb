4e1025672d26a986874c854c1ff314eaa4c5c88c
Alfredo Cassanje <alfredocassanj@gmail.com>
2026-08-14 04:25:47 +0100

    Epasb

--- epasb/assets/main.js
+++ epasb/assets/main.js
@@ -68,6 +68,7 @@ document.addEventListener('DOMContentLoaded', () => {
       modal.addEventListener('click', (e) => {
         if (e.target === modal) closeModal(modal);
       });
+      
       const closeBtn = modal.querySelector('.modal-close');
       if (closeBtn) closeBtn.addEventListener('click', () => closeModal(modal));
     });
